5d50af35e26f02fc71f7ba84167bd8de36c81975
jackokeefe11 <37940238+jackokeefe11@users.noreply.github.com>
2025-09-28 16:52:51 -0700

    full viewport support

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Home from './pages/Home';
-import About from './pages/About';
-import Writing from './pages/Writing';
-import Visual from './pages/Visual';
-import Research from './pages/Research';
+import PaperBackground from './components/PaperBackground';
 import './App.css';
-
-function AppContent() {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
-  return (
-    <div className="App">
-      <Navigation />
-      {isHome ? (
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      ) : (
-        <main className="main-content">
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/writing" element={<Writing />} />
-            <Route path="/visual" element={<Visual />} />
-            <Route path="/research" element={<Research />} />
-          </Routes>
-        </main>
-      )}
-    </div>
-  );
-}
 
 function App() {
   return (
-    <Router>
-      <AppContent />
-    </Router>
+    <div className="App">
+      <PaperBackground />
+    </div>
   );
 }
 
